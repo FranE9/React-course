4d12495d144163de6b180fc30fb11340161c6ba1
Francisco Cornejo <00105018@uca.edu.sv>
2023-08-22 00:46:15 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import { Greetings, UserCard } from "./Greetings";
 import {Product} from "./Product";
@@ -9,24 +9,37 @@ import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id:1,
-    name: 'Ryan ray',
-    image: 'https://robohash.org/user2'
-  },
-  {
-    id:2,
-    name: 'Gordon Hash',
-    image: 'https://robohash.org/user3'
-  }
-]
+
+function Counter (){
+
+  const [counter, setCounter] = useState(0)
+  return (
+    <div>
+    <h1>Counter: {counter}</h1>
+    <button onClick={() =>{
+      setCounter(counter + 1)
+    }}>Sumar</button>
+
+    <button onClick={() =>{
+      setCounter(counter - 1)
+    }}>Restar</button>
+
+<button onClick={() =>{
+      setCounter( 0)
+    }}>Reiniciar</button>
+
+    
+    </div>
+
+    
+  )
+}
+
+
 
 root.render(
   <>
-{users.map( (user, i) => {
-  return <h1 key ={i}>{user.name} </h1>
-})}
- 
+
+ <Counter/>
   </>
 );
